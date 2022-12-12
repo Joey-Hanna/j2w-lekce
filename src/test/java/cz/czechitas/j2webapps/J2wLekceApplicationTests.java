883f59f812024aacce9f87252a4b8b572a9fb576
Joey-Hanna <joey.martin.de.lange@gmail.com>
2022-12-12 19:01:11 +0100

    priprava k render

---
+++ src/test/java/cz/czechitas/j2webapps/J2wLekceApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.czechitas.j2webapps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J2wLekceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
