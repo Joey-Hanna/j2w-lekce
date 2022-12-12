188969b61798a551e35f621c1ec02a1ed9e57618
Joey-Hanna <joey.martin.de.lange@gmail.com>
2022-12-12 18:56:15 +0100

    Initial commit

---
+++ src/main/java/cz/czechitas/j2webapps/J2wLekceApplication.java
@@ -0,0 +1,13 @@
+package cz.czechitas.j2webapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J2wLekceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(J2wLekceApplication.class, args);
+	}
+
+}
